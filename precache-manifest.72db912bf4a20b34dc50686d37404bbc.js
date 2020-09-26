ed814facc850b0696f9ab6a31381d68187859def
barthezzko <barthezzko@gmail.com>
2020-09-25 23:59:15 -0400

    Updates

---
+++ precache-manifest.72db912bf4a20b34dc50686d37404bbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d46f9412be8970e9e4bbe5bdf8cd02b",
+    "url": "/react-ui/index.html"
+  },
+  {
+    "revision": "d8c302a4ae870786d774",
+    "url": "/react-ui/static/css/main.bdff84e1.chunk.css"
+  },
+  {
+    "revision": "6a2ee4255b20b42d0e5f",
+    "url": "/react-ui/static/js/2.797eedef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ui/static/js/2.797eedef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c302a4ae870786d774",
+    "url": "/react-ui/static/js/main.72a172cf.chunk.js"
+  },
+  {
+    "revision": "22eb3fc9e5fb0185ffdb",
+    "url": "/react-ui/static/js/runtime-main.2cd7f2f0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-ui/static/media/logo.ee7cd8ed.svg"
+  }
+]);
