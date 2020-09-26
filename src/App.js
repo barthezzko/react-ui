343e6cec5e9bb4410379c5d3c84ee780d69337fd
barthezzko <barthezzko@gmail.com>
2020-09-25 23:51:59 -0400

    initial commit from react-create

--- src/App.js
+++ src/App.js
@@ -10,19 +10,8 @@ function App() {
       <header className="App-header">
         <Welcome name="Mike"/>
         <Table value="asdf"/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -3,7 +3,13 @@ import Cell from './Cell.js';
 
 export default class Row extends React.Component {
   render() {
-    return <tr><Cell value={this.props.value}/></tr>;
+    return <tr>
+                <Cell value={this.props.value}/>
+                <Cell value={this.props.value}/>
+                <Cell value={this.props.value}/>
+                <Cell value={this.props.value}/>
+                <Cell value={this.props.value}/>
+            </tr>;
   }
 }
 
